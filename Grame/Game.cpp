03160a36b8fd219bab207f6f59c12472026ad601
CyborgCabbage <finlaylaidlaw9@gmail.com>
2023-09-29 17:27:46 +0100

    Add SDL2 libs back in

--- Grame/Game.cpp
+++ Grame/Game.cpp
@@ -15,14 +15,14 @@ Game::Game()
 
 void Game::Update(double dt)
 {
-	std::cout << "Update " << dt << "\n";
+	//std::cout << "Update " << dt << "\n";
 }
 
 void Game::Render(double dt)
 {
-	static double a{ 0.0 };
+	/*static double a{0.0};
 	a += dt;
 	static double b{ 0.0 };
 	b += 1.0;
-	std::cout << a / b << "\n";
+	std::cout << a / b << "\n";*/
 }
